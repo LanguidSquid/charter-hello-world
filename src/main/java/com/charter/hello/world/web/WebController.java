2c0e0b80204474ace67578dc81e1eafca4eabb85
Satish <satish.embadi@senecaglobal.com>
2020-02-25 08:19:25 +0530

    changes made as per req

--- src/main/java/com/charter/hello/world/web/WebController.java
+++ src/main/java/com/charter/hello/world/web/WebController.java
@@ -11,6 +11,51 @@ public class WebController {
             consumes = {"application/json"},
             produces = {"application/json"})
     @ResponseBody HelloWorldResponse hello(){
-        return new HelloWorldResponse("Hello World!");
+        return new HelloWorldResponse(getMessage());
     }
+
+    @PostMapping(value = "/hello",
+            consumes = {"application/json"},
+            produces = {"application/json"})
+    @ResponseBody HelloWorldResponse postHello(@RequestBody HelloWorldResponse helloWorldResponse){
+        setMessage(helloWorldResponse.getMessage());
+        return helloWorldResponse;
+    }
+
+    private String getMessage() {
+        String message = null;
+        try {
+            message = readFromInputStream(new FileInputStream(ResourceUtils.getFile("classpath:message.txt")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(StringUtils.isEmpty(message)) {
+            return "Hello World!";
+        }
+        return message;
+    }
+
+    private String readFromInputStream(InputStream inputStream)
+            throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br
+                     = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line);
+            }
+        }
+        return resultStringBuilder.toString();
+    }
+
+    private void setMessage(String message) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(ResourceUtils.getFile("classpath:message.txt")))) {
+            writer.write(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
